55c6d78723394ca33b4ab85e061e24595c674eab
perseus.algol <perseus.algol2@gmail.com>
2023-08-09 10:27:25 +0300

    - Added trpc-openapi, swagger-ui-express; - added REST api; - autogenerated swagger docs;

--- back/src/index.ts
+++ back/src/index.ts
@@ -3,15 +3,28 @@ import * as trpcExpress from '@trpc/server/adapters/express';
 import express, { Request, Response } from 'express';
 import { appRouter } from './router';
 import { Context, createContext } from './context';
+import { createOpenApiExpressMiddleware } from 'trpc-openapi';
+import { generateOpenApiDocument } from 'trpc-openapi';
+import swaggerUi from 'swagger-ui-express';
+
+const openApiDocument = generateOpenApiDocument(appRouter, {
+  title: 'tRPC OpenAPI',
+  version: '1.0.0',
+  baseUrl: 'http://localhost:4000/api',
+});
 
 const app = express();
 
-app.use(
-  '/trpc',
-  trpcExpress.createExpressMiddleware({
-    router: appRouter,
-    createContext,
-  }),
-);
+app.use('/trpc', trpcExpress.createExpressMiddleware({
+  router: appRouter,
+  createContext,
+}));
+
+app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(openApiDocument));
+
+app.use('/api', createOpenApiExpressMiddleware({
+  router: appRouter,
+  createContext, 
+}));
 
 app.listen(4000);
--- back/src/router.ts
+++ back/src/router.ts
@@ -5,31 +5,42 @@ import { Context } from './context';
 import { User } from './entities/types';
 import schemas from './entities/schemas';
 import { users } from './mock-data';
+import { OpenApiMeta } from 'trpc-openapi';
 
 type Opts<Input, Context> = {
   input: Input,
   ctx: Context,
 }
 
-const t = initTRPC.context<Context>().create();
+const t = initTRPC
+  .meta<OpenApiMeta>()
+  .context<Context>()
+  .create();
 
 const userList = t.procedure
+  .meta({ openapi: { method: 'GET', path: '/users' } })
+  .input(z.undefined())
+  .output(z.array(schemas.user))
   .query(async () => {
     // Retrieve users from a datasource, this is an imaginary database
     return users;
   });
 
 const userById = t.procedure
-  .input(z.string())
+  .meta({ openapi: { method: 'GET', path: '/users/{id}' } })
+  .input(z.object({id: z.string()}))
+  .output(z.optional(schemas.user))
   .query(async (opts) => {
     const { input } = opts;
     // Retrieve the user with the given ID
-    const user: User | undefined = users.find(u => u.id === input);
+    const user: User | undefined = users.find(u => u.id === input.id);
     return user;
   });
 
 const userCreate = t.procedure
+  .meta({ openapi: { method: 'POST', path: '/users' } })
   .input(schemas.user)
+  .output(schemas.user)
   .mutation(async (opts: Opts<User, Context>) => {
     const { input, ctx } = opts;
     // Create a new user in the database
